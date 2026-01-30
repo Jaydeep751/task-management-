1a0e948a1ebd4467321fe88f472105c44b730745
bhendarada jaydeep <rambhedarada@gimail.com>
2026-01-30 13:48:09 +0530

    [BJ][messages]

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './compoinents/Header'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./compoinents/Header";
+import Register from "./pages/Register";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header/>
+      <Header />
+      <Register />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
