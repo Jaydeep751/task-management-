9fa6296368b3190b17366677778bf4342e88c49b
bhendarada jaydeep <rambhedarada@gimail.com>
2026-01-31 12:59:47 +0530

    [BJ][messages]

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,11 @@
-import React from "react";
-import "./Register.css";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
-const Register = () => {
+const Login = () => {
   return (
     <div className="form-container">
-      <h1 className="form-title">REGISTER</h1>
+      <h1 className="form-title">Welcome Back</h1>
       <form>
-        <div className="form-group">
-          <label htmlFor="name">Full Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Enter your full name"
-          />
-        </div>
-
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
           <input
@@ -25,17 +15,6 @@ const Register = () => {
             placeholder="Enter your email"
           />
         </div>
-
-        <div className="form-group">
-          <label htmlFor="phone">Phone Number</label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            placeholder="Enter your phone number"
-          />
-        </div>
-
         <div className="form-group">
           <label htmlFor="password">Password</label>
           <input
@@ -45,14 +24,12 @@ const Register = () => {
             placeholder="Enter your password"
           />
         </div>
-
         <button type="submit" className="btn-primary">
-          Register
+          Submit
         </button>
-       
       </form>
     </div>
   );
 };
 
-export default Register;
+export default Login;
