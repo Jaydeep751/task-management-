e6a540627594c9a766b38b83198de02da591f212
bhendarada jaydeep <rambhedarada@gimail.com>
2026-02-03 09:09:59 +0530

    [BJ][messages]

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,45 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./compoinents/Header";
+//import Header from "./components/Header";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+
+const DefaultRoute = () => {
+  const authData = JSON.parse(localStorage.getItem("authData"));
+  if (authData) {
+    return <Navigate to="/login" replace />;
+  }
+  return <Navigate to="/register" replace />;
+};
 
 function App() {
   const route = createBrowserRouter([
-    
     {
-    path: "/login",
-    element:<Login/>
+      path: "/",
+      element: <DefaultRoute />,
     },
     {
-      path:"/register",
-      element:<Register/>
-    }
-
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
+    },
   ]);
 
   return <RouterProvider router={route} />;
 }
+
 export default App;
