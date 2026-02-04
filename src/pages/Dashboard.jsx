f7d536accb96987f6976a1cf99fe689e44ac5cdf
bhendarada jaydeep <rambhedarada@gimail.com>
2026-02-04 13:41:19 +0530

    [BJ][messages]

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import TaskList from "../components/TaskList";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/tasks");
+      const data = response.json();
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleLogout = () => {
     localStorage.removeItem("loginData");
     localStorage.removeItem("authData");
-    //localStorage.clear();
-    navigate("/Login");
+    // localStorage.clear()
+    navigate("/login");
   };
 
   return (
